0892aefcd3b6ff0a02c6199bda54767667ec9886
Tarun Tailor <taruntailor7>
2025-02-22 17:50:45 +0530

    feat: websocket server created

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import http from 'http';  
+dotenv.config(); 
+const port = process.env.PORT || 3000; 
 
 const app = express();
-const port = 3000; 
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', () => {
+  console.log('Client connected...');
+})
 
 // Define a route
 app.get('/', (req, res) => {
